3f55acefc7f5576bc121bb9f457d452af1d9bffe
Serhii Konovalenko <yourskif@gmail.com>
2025-09-03 13:28:39 +0300

    Remove console-online-store from main branch

--- console-online-store/StoreDAL/Interfaces/ICategoryRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace StoreDAL.Interfaces;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using StoreDAL.Entities;
-
-public interface ICategoryRepository : IRepository<Category>
-{
-}
