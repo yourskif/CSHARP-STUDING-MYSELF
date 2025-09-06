b26b39e274b09eb8cf4a1f8f8c08792a6af00fb4
Serhii Konovalenko <yourskif@gmail.com>
2025-09-06 06:29:36 +0300

    new multy level project begin

---
+++ StoreBLL/Models/CustomerOrderModel.cs
@@ -0,0 +1,26 @@
+﻿namespace StoreBLL.Models
+{
+    public class CustomerOrderModel : AbstractModel
+    {
+        public CustomerOrderModel() : base()
+        {
+        }
+
+        public CustomerOrderModel(int id, int userId, string operationTime, int orderStateId)
+            : base(id)
+        {
+            this.UserId = userId;
+            this.OperationTime = operationTime;
+            this.OrderStateId = orderStateId;
+        }
+
+        public int UserId { get; set; }
+        public string OperationTime { get; set; }
+        public int OrderStateId { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {this.Id}, UserId: {this.UserId}, Time: {this.OperationTime}, StateId: {this.OrderStateId}";
+        }
+    }
+}
---
+++ StoreBLL/Models/OrderDetailModel.cs
@@ -0,0 +1,28 @@
+﻿namespace StoreBLL.Models
+{
+    public class OrderDetailModel : AbstractModel
+    {
+        public OrderDetailModel() : base()
+        {
+        }
+
+        public OrderDetailModel(int id, int orderId, int productId, decimal price, int amount)
+            : base(id)
+        {
+            this.OrderId = orderId;
+            this.ProductId = productId;
+            this.Price = price;
+            this.ProductAmount = amount;
+        }
+
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public decimal Price { get; set; }
+        public int ProductAmount { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {this.Id}, OrderId: {this.OrderId}, ProductId: {this.ProductId}, Price: {this.Price:C}, Amount: {this.ProductAmount}";
+        }
+    }
+}
