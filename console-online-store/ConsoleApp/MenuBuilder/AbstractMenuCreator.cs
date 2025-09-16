6d72468d9a6fd68f449fdf28bb980bbf953f8810
Serhii Konovalenko <yourskif@gmail.com>
2025-09-16 21:42:22 +0300

    step2: Complete file reorganization

--- console-online-store/ConsoleApp/MenuBuilder/AbstractMenuCreator.cs
+++ console-online-store/ConsoleApp/MenuBuilder/AbstractMenuCreator.cs
@@ -1,12 +1,1 @@
-using StoreDAL.Data;
-
-namespace ConsoleMenu.Builder;
-public abstract class AbstractMenuCreator : IMenuCreator
-{
-    public abstract (ConsoleKey id, string caption, Action action)[] GetMenuItems(StoreDbContext context);
-
-    public Menu Create(StoreDbContext context)
-    {
-        return new Menu(this.GetMenuItems(context));
-    }
-}
+﻿
--- console-online-store/ConsoleApp/MenuBuilder/IMenuCreator.cs
+++ console-online-store/ConsoleApp/MenuBuilder/IMenuCreator.cs
@@ -1,11 +1,1 @@
-/*
-Yuriy Antonov copyright 2018-2020
-*/
-using StoreDAL.Data;
-
-namespace ConsoleMenu.Builder;
-
-public interface IMenuCreator
-{
-    Menu Create(StoreDbContext context);
-}
+﻿
