0be6958d0cb990fcf4daaca361a3aa76dfa71538
Serhii Konovalenko <yourskif@gmail.com>
2025-09-25 10:04:45 +0300

    chore: cleanup — drop nested console-online-store mirror; refine .gitignore

--- console-online-store/StoreDAL/Interfaces/IManufacturerRepository .cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StoreDAL.Interfaces;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-using StoreDAL.Entities;
-
-public interface IManufacturerRepository : IRepository<Manufacturer>
-{
-}
