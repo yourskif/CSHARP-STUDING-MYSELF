e4cf1fcfeb0f17a7f0437a638eaf9fec690cb241
Serhii Konovalenko <yourskif@gmail.com>
2025-09-26 06:39:00 +0300

    chore: track ICustomerOrderService and StoreDbContext

---
+++ console-online-store/StoreBLL/Interfaces/ICustomerOrderService.cs
@@ -0,0 +1,16 @@
+﻿// Path: C:\Users\SK\source\repos\C#\1414\console-online-store\StoreBLL\Interfaces\ICustomerOrderService.cs
+namespace StoreBLL.Interfaces;
+
+using System.Collections.Generic;
+using StoreBLL.Models;
+
+public interface ICustomerOrderService : ICrud
+{
+    bool TryChangeState(int orderId, int newStateId, out string error);
+
+    bool CancelOwnOrder(int orderId, int userId, out string error);
+
+    bool MarkAsReceived(int orderId, int userId, out string error);
+
+    IEnumerable<CustomerOrderModel> GetOrdersByUser(int userId);
+}
